d072c187fa6fc161fc541d8e2bc7b813b6f43c63
Jacky Li <jackylihk123@gmail.com>
2023-08-20 16:33:31 -0700

    added footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,12 +21,12 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
-        <div className="hero__image-container">
-          <div className="hero__image">
-            <Image src="/hero.png" alt="hero" fill className="object-contain" />
-            <div className="hero__image-overlay" />
-          </div>
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
